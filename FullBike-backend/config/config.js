fbe8b258e949f8d147d4964877f15181e6f58217
Leidy <leidymarbu@gmail.com>
2025-02-13 11:04:07 -0300

    Update Sequelize 6 configuration

--- FullBike-backend/config/config.js
+++ FullBike-backend/config/config.js
@@ -1,35 +1,58 @@
-// Used to configure the database connection
+// // Used to configure the database connection
+// require('dotenv').config();
+
+// module.exports = {
+//     development: {
+//         url: process.env.DB_URI,
+//         // username: process.env.DB_USER,
+//         // database: process.env.DB_NAME,
+//         // host: process.env.DB_HOST,
+//         // port: process.env.DB_PORT || 5432,
+//         dialect: 'postgres'
+//     },
+//     test: {
+//         url: process.env.DB_URI,
+//         // username: process.env.DB_USER,
+//         // database: process.env.DB_NAME,
+//         // host: process.env.DB_HOST,
+//         // port: process.env.DB_PORT || 5432,
+//         dialect: 'postgres'
+//     },
+//     production: {
+//         url: process.env.DATABASE_URL,
+//         // username: process.env.DB_USER,
+//         // database: process.env.DB_NAME,
+//         // host: process.env.DB_HOST,
+//         // port: process.env.DB_PORT || 5432,
+//         dialect: 'postgres',
+//         dialectOptions: {
+//             ssl: {
+//                 require: true,
+//                 rejectUnauthorized: false
+//             }
+//         }
+//     }
+// }
+
 require('dotenv').config();
 
 module.exports = {
     development: {
-        url: process.env.DB_URI,
-        // username: process.env.DB_USER,
-        // database: process.env.DB_NAME,
-        // host: process.env.DB_HOST,
-        // port: process.env.DB_PORT || 5432,
+        use_env_variable: 'DB_URI',
         dialect: 'postgres'
     },
     test: {
-        url: process.env.DB_URI,
-        // username: process.env.DB_USER,
-        // database: process.env.DB_NAME,
-        // host: process.env.DB_HOST,
-        // port: process.env.DB_PORT || 5432,
+        use_env_variable: 'DB_URI',
         dialect: 'postgres'
     },
     production: {
-        url: process.env.DATABASE_URL,
-        // username: process.env.DB_USER,
-        // database: process.env.DB_NAME,
-        // host: process.env.DB_HOST,
-        // port: process.env.DB_PORT || 5432,
+        use_env_variable: 'DATABASE_URL',
         dialect: 'postgres',
         dialectOptions: {
             ssl: {
                 require: true,
-                rejectUnauthorized: false
+                rejectUnauthorized: false // Prevents SSL connection issues
             }
         }
     }
-}
+};
